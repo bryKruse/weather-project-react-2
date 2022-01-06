0021d00c3ab574d31a2347bd131fe321367f2237
bryKruse <90730678+bryKruse@users.noreply.github.com>
2022-01-06 14:15:18 -0600

    Removed conversion

--- src/WeatherTemperature.js
+++ src/WeatherTemperature.js
@@ -16,25 +16,7 @@ export default function WeatherTemperature(props) {
     return (
       <span className="WeatherTermperature">
         <span className="temperature">{Math.round(props.celsius)}</span>
-        <span className="unit">
-          °C |
-          <a href="/" onClick={showFahrenheit}>
-            °F
-          </a>{" "}
-        </span>
-      </span>
-    );
-  } else {
-    let fahrenheit = (props.celsius * 9) / 5 + 32;
-    return (
-      <span className="WeatherTermperature">
-        <span className="temperature">{Math.round(fahrenheit)}</span>
-        <span className="unit">
-          <a href="/" onClick={showCelsius}>
-            °C{""}
-          </a>{" "}
-          | °F{" "}
-        </span>
+        <span className="unit">°C </span>
       </span>
     );
   }
