fec48da672b6c0ca8becffd7da5dcd578eaf88fd
bryKruse <90730678+bryKruse@users.noreply.github.com>
2022-01-06 14:18:52 -0600

    Update WeatherTemperature.js

--- src/WeatherTemperature.js
+++ src/WeatherTemperature.js
@@ -2,15 +2,6 @@ import React, { useState } from "react";
 
 export default function WeatherTemperature(props) {
   const [unit, setUnit] = useState("celsius");
-  function showFahrenheit(event) {
-    event.preventDefault();
-    setUnit("fahrenheit");
-  }
-
-  function showCelsius(event) {
-    event.preventDefault();
-    setUnit("celsius");
-  }
 
   if (unit === "celsius") {
     return (
