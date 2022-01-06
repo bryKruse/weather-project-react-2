ce99862c97f7c2ac505c71890924f10b28c45cfe
bryKruse <90730678+bryKruse@users.noreply.github.com>
2022-01-05 21:50:53 -0600

    Added footer - source to GitHub

--- src/App.js
+++ src/App.js
@@ -4,6 +4,13 @@ export default function App() {
   return (
     <div className="App">
       Weather App
+      <footer>
+        This project was coded by Brynn Kruse, and it is{" "}
+        <a href="https://github.com/bryKruse/weather-project-react-2" target="_blank" rel="noreferrer">
+          open-sourced on GitHub
+        </a>
+        .
+      </footer>
     </div>
   );
 }
