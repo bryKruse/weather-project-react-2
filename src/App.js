f42ebb136f8e93345864fe58074864dd0a79b252
bryKruse <90730678+bryKruse@users.noreply.github.com>
2022-01-05 21:57:15 -0600

    Add linkedin and edit format

--- src/App.js
+++ src/App.js
@@ -3,9 +3,11 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-      Weather App
+      <h1>Weather App</h1>
       <footer>
-        This project was coded by Brynn Kruse, and it is{" "}
+        This project was coded by{" "} 
+        <a href="https://www.linkedin.com/in/brynn-kruse-0b154017b/" target="_blank" rel="noreferrer">Brynn Kruse</a>
+        , and it is{" "}
         <a href="https://github.com/bryKruse/weather-project-react-2" target="_blank" rel="noreferrer">
           open-sourced on GitHub
         </a>
