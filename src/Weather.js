1334e4a1595681cc40f9ee365dde4742772224f4
bryKruse <90730678+bryKruse@users.noreply.github.com>
2022-01-06 12:11:21 -0600

    CSS

--- src/Weather.js
+++ src/Weather.js
@@ -22,15 +22,13 @@ export default function Weather(){
             <div className="row mt-3">
                 <div className="col-6">
                     <div className="clearfix">
-                    <img 
-                    src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png" 
-                    alt="partly cloudy"
-                    className="float-left" 
-                    />
-                    <div className="float-left">
-                    <span className="current-temperature">3</span>
-                    <span className="unit">°C</span>
-                    </div>
+                        <img 
+                            src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png" 
+                            alt="partly cloudy"
+                            className="float-left" 
+                            />
+                        <span className="current-temperature">3</span>
+                        <span className="unit">°C</span>
                     </div>
                 </div>
                 <div className="col-6">
